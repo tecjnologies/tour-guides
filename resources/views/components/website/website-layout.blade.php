2b3f9e4a392007519d597d90b18e8c2ef184e2d8
Irfan Karem <irfan@technologies.ae>
2024-09-26 16:24:01 +0400

    changes reources folder public

--- resources/views/components/website/website-layout.blade.php
+++ resources/views/components/website/website-layout.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title', 'Tour Guide - Homepage')</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['/public/resources/css/app.css', '/public/resources/js/app.js'])
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="{{ asset('/public/assets/js/jquery.min.js') }}"></script>
         <script src="{{ asset('/public/assets/plugins/slick/slick.js') }}"></script>
