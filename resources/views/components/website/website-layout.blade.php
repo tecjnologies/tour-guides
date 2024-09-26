10c93c463400907af5dc6760e68017e1166fe74d
Irfan Karem <irfan@technologies.ae>
2024-09-26 16:34:10 +0400

    custom.css

--- resources/views/components/website/website-layout.blade.php
+++ resources/views/components/website/website-layout.blade.php
@@ -10,7 +10,7 @@
         <script src="{{ asset('/public/assets/js/jquery.min.js') }}"></script>
         <script src="{{ asset('/public/assets/plugins/slick/slick.js') }}"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-        <link rel="stylesheet" href="assets/css/custom.css">
+        <link rel="stylesheet" href="/public/assets/css/custom.css">
         @stack('css')
     </head>
     <body>
