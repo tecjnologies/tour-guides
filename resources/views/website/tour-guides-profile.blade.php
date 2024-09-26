19299976cee2c42c0c32138097e44dd853ae3325
Irfan Karem <irfan@technologies.ae>
2024-09-26 10:10:28 +0400

    merging and fixes of conflicts

--- resources/views/website/tour-guides-profile.blade.php
+++ resources/views/website/tour-guides-profile.blade.php
@@ -1,19 +1,10 @@
-@push('css')
-@endpush
 <x-website-layout>
     @section('title', 'Tour Guide Profile')
     <div class="mx-auto">
         <x-home.tour-guide-grid class="h-12 w-auto" />
     </div>
 </x-website-layout>
-<<<<<<< .mine
 
 
 
 
-=======
-            </div>
-        </div>
-    </div>
-</x-website-layout>
->>>>>>> .theirs
