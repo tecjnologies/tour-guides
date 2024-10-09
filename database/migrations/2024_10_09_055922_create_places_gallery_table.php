dfa5034adef6e8a5accd8b56d636bc891c2c4c9e
Irfan Karem <irfan@technologies.ae>
2024-10-09 11:16:02 +0400

    dashboard| migration unversioned

---
+++ database/migrations/2024_10_09_055922_create_places_gallery_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('places_gallery', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('place_id');
+            $table->string('image');
+            $table->timestamps();
+
+            $table->foreign('place_id')->references('id')->on('places')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('places_gallery');
+    }
+};
