a35242664cf494c13d29d6668a9aa305be57ed35
Irfan Karem <irfan@technologies.ae>
2024-12-17 10:44:38 +0400

    website | tabs | fix in tabs logic

--- resources/views/website/destinations.blade.php
+++ resources/views/website/destinations.blade.php
@@ -1,5 +1,5 @@
 <style>
-    textarea:focus, select:focus{
+    textarea:focus, select:focus {
         outline: unset !important;  
         --tw-ring-color: unset !important;
         --tw-ring-shadow: unset !important;
@@ -10,60 +10,75 @@
     @section('title', 'Tour Guide - Destinations')
     <div class="row px-5">
         <div class="col-md-12 d-flex justify-content-between align-items-center">
+            {{-- Tabs --}}
             <div class="tabs">
                 <ul class="nav nav-tabs" id="destinationsTab" role="tablist">
-                    @forelse ($placeTypes as $type)
-                        @if( $type->places->count() > 0)
+                    @php $activeTabSet = false; @endphp
+                    @foreach ($placeTypes as $type)
+                        @if ($type->places->isNotEmpty())
                             <li class="nav-item" role="presentation">
-                                <button class="nav-link active" id="type-{{$type->id}}-tab" data-bs-toggle="tab" data-bs-target="#type-{{$type->id}}" type="button" role="tab" 
-                                aria-controls="type-{{$type->id}}" aria-selected="true"> {{ $type->name }} </button>
+                                <button class="nav-link {{ !$activeTabSet ? 'active' : '' }}" 
+                                    id="type-{{ $type->id }}-tab" 
+                                    data-bs-toggle="tab" 
+                                    data-bs-target="#type-{{ $type->id }}" 
+                                    type="button" 
+                                    role="tab" 
+                                    aria-controls="type-{{ $type->id }}" 
+                                    aria-selected="{{ !$activeTabSet ? 'true' : 'false' }}">
+                                    {{ $type->name }}
+                                </button>
+                                @php $activeTabSet = true; @endphp
                             </li>
-                        @else 
                         @endif
-                    @empty
-                    @endforelse
-                  </ul>
+                    @endforeach
+                </ul>
             </div>
+
+            {{-- Filters --}}
             <div class="filters d-flex justify-content-end align-items-center">
+                {{-- Emirates Filter --}}
                 <div class="emirates d-flex justify-content-between align-items-center rounded-md px-3 py-2 border border-gray-300 me-md-3">
-                        <img src="{{ asset('assets/images/icons/sort.svg') }}" alt="sort-icon" class="sort-icon me-2" />
-                        <select id="emirates" name="emirates" class="w-full  border-0">
-                            <option value="" class="font-5 displaty-14 color-blue">All Emirate</option>
-                            <option value="abu_dhabi" class="font-5 displaty-14 color-blue">Abu Dhabi</option>
-                            <option value="dubai" class="font-5 displaty-14 color-blue">Dubai</option>
-                            <option value="sharjah" class="font-5 displaty-14 color-blue">Sharjah</option>
-                            <option value="umm_al_quwain" class="font-5 displaty-14 color-blue">Umm al Quwain</option>
-                            <option value="ajman" class="font-5 displaty-14 color-blue">Ajman</option>
-                            <option value="ras_al_khaimah" class="font-5 displaty-14 color-blue">Ras al Khaimah</option>
-                        </select>
+                    <img src="{{ asset('assets/images/icons/sort.svg') }}" alt="sort-icon" class="sort-icon me-2" />
+                    <select id="emirates" name="emirates" class="w-full border-0">
+                        <option value="">All Emirate</option>
+                        <option value="abu_dhabi">Abu Dhabi</option>
+                        <option value="dubai">Dubai</option>
+                        <option value="sharjah">Sharjah</option>
+                        <option value="umm_al_quwain">Umm al Quwain</option>
+                        <option value="ajman">Ajman</option>
+                        <option value="ras_al_khaimah">Ras al Khaimah</option>
+                    </select>
                 </div>
-                <div class="sort d-flex justify-content-between align-items-center rounded-md border px-3 py-2 border-gray-300">
-                    <img src="{{ asset('assets/images/icons/sort.svg') }}" alt="sort-icon"  class=" sort-icon me-2"/>
+
+                {{-- Sort Filter --}}
+                <div class="sort d-flex justify-content-between align-items-center rounded-md px-3 py-2 border border-gray-300">
+                    <img src="{{ asset('assets/images/icons/sort.svg') }}" alt="sort-icon" class="sort-icon me-2" />
                     <select name="sort" class="w-full border-0">
-                        <option value="" class="font-5 displaty-14 color-blue">Sort results </option>
-                        <option value="asc" class="font-5 displaty-14 color-blue"> Ascending </option>
-                        <option value="desc" class="font-5 displaty-14 color-blue"> Descending </option>
+                        <option value="">Sort results</option>
+                        <option value="asc">Ascending</option>
+                        <option value="desc">Descending</option>
                     </select>
                 </div>
             </div>
         </div>
+
+        {{-- Tab Content --}}
         <div class="col-md-12">
             <div class="tab-content" id="destinationsTabContent">
-                @forelse ($placeTypes as $type)
-                    @forelse ($type->places as $place)
-                        @if($place->placetype_id === $type->id)
-                            <div class="tab-pane fade show active" id="type-{{$place->placetype_id}}" role="tabpanel" 
-                                aria-labelledby="type-{{$place->placetype_id}}-tab"> 
-                                
-                                {{ $place->name }} 
-                            
-                            </div>                        
-                        @else
-                        @endif
-                    @empty
-                    @endforelse
-                @empty
-                @endforelse            
+                @php $activePaneSet = false; @endphp
+                @foreach ($placeTypes as $type)
+                    @if ($type->places->isNotEmpty())
+                        <div class="tab-pane fade {{ !$activePaneSet ? 'show active' : '' }}" 
+                             id="type-{{ $type->id }}" 
+                             role="tabpanel" 
+                             aria-labelledby="type-{{ $type->id }}-tab">
+                            @foreach ($type->places as $place)
+                                <div>{{ $place->name }}</div>
+                            @endforeach
+                        </div>
+                        @php $activePaneSet = true; @endphp
+                    @endif
+                @endforeach
             </div>
         </div>
     </div>
